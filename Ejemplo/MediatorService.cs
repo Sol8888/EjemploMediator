ecc529512a88a79b1f292f2a159eaaef098db717
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-06-17 18:34:15 -0500

    Agregar archivos de proyecto.

---
+++ Ejemplo/MediatorService.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejemplo
+{
+    public class MediatorService
+    {
+        public event Action<string>? OnNotificar;
+
+        public void Notificar(string mensaje)
+        {
+            OnNotificar?.Invoke(mensaje);
+        }
+    }
+}
