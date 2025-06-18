71729c96e826ddd15526a5057ad72d38ea52fce7
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-06-17 19:18:25 -0500

    2 emisores, 1 receptor

--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddSingleton<MediatorService>();
+builder.Services.AddSingleton<Ejemplo.Services.MediatorService>();
 
 var app = builder.Build();
 
---
+++ Ejemplo/Services/MediatorService.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejemplo.Services
+{
+    public class MediatorService
+    {
+        public event Action<string>? OnMensaje;
+
+        public void Enviar(string mensaje)
+        {
+            OnMensaje?.Invoke(mensaje);
+        }
+    }
+}
